fa98a9336bd46b02732c6174aa84c8be62ee0f4d
Henry Bravo <henry.bravo89@gmail.com>
2018-06-02 18:29:07 -0500

    Allowed host

--- geekfts_shop/settings.py
+++ geekfts_shop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ["SECRET_KEY_GEEKFTS"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['45.79.102.80', 'www.geekfts.com']
 
 
 # Application definition
